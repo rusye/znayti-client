9f5ef74af75616066e8654ad1d0feffb056bd121
Russ yev <ruslanyev@gmail.com>
2019-02-28 20:29:00 -0800

    included the router switch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
-import Router from './components/Router'
-// import App from './components/App';
+import HomePage from './components/HomePage';
+import Categories from './components/Categories';
+// import Business from './components/Business';
+// import Category from './components/Category';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/business/search/' component={Categories} />
+          {/* I need the category to display only category that the user selects after the search response */}
+          {/* <Route exact path='/business/search/--idk what the rest is--' component={Category} /> */}
+          {/* <Route exact path='/business/:id' component={Business} /> */}
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
