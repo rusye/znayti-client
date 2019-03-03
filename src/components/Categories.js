db4d67009598824d9f872d5dd45cca65470bfe40
Russ yev <ruslanyev@gmail.com>
2019-03-03 01:33:37 -0800

    able to get results back from server

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Categories.css';
 import Search from './Search';
+import { normalizeResponseErrors } from '../functions/normalizeResponse';
+const {API_BASE_URL} = require('../config');
 
-class Categories extends Component {
 
-    // 4. Get request for backend data
-
-  render() {
-    return (
-      <div className='categories'>
-        <Search />
-      </div>
-    );
+export default function Categories(props) {
+  const [categories, setCategories] = useState("");
+  // 1. Get request for backend data
+  const fetchCategories = async () => {
+    const response = await fetch(`${API_BASE_URL}${props.location.pathname}${props.location.search}`)
+    const normalize = await normalizeResponseErrors(response)
+    const rcvdCategories = await normalize.json()
+    let newArr = []
+    rcvdCategories.forEach(category => newArr.push(category.charAt(0).toUpperCase() + category.slice(1)))
+    setCategories(newArr)
+    // How do make it wait for the response to finish and then do the if statement?
   }
-}
+  
+  useEffect(
+    () => {
+      fetchCategories()
+    }, []
+  )
+  // 2. Display Results
+  let category;
+  let title;
+  if (categories.length > 0) {
+    title = 'Categories';
+    category = categories.map((category, index) => {
+      return (
+        <button type='button' key={index}>{category}</button>
+      )
+    })
+  } else {
+    title = 'No businesses in this area'
+    category = 'Submit a business'
+  }
 
-export default Categories;
+  
+  // 3. Handle click on category
+
+  return (
+    <div className='categories'>
+      <Search />
+      <div>
+        <h2>{title}</h2>
+        {/* Div will show if  */}
+        <div>{category}</div>
+      </div>
+    </div>
+  );
+}
