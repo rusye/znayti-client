dcdac4877fe12ab922bb88576f1d1cad242c4074
Russ yev <ruslanyev@gmail.com>
2019-03-03 15:19:38 -0800

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './NavBar.css';
+
+export default function Search(props) {
+
+  return (
+    <h2>This Is The Nav Bar</h2>
+  )
+}
