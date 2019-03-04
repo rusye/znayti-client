76e98adfeb8a59c9aae402a5f7c33b696f24c300
Russ yev <ruslanyev@gmail.com>
2019-03-03 19:19:00 -0800

    updated function name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './NavBar.css';
 
-export default function Search(props) {
+export default function NavBar(props) {
 
   return (
     <h2>This Is The Nav Bar</h2>
