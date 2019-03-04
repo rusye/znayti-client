daaaa6302cbdeade80015d4ebb01519fe1c31c3f
Russ yev <ruslanyev@gmail.com>
2019-03-04 00:05:50 -0800

    removed notes, added onClick for business search

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,16 +9,11 @@ const {API_BASE_URL} = require('../config');
 export default function Businesses(props) {
   const [businesses, setBusinesses] = useState('');
   const [catName, setCatName] = useState('');
-  // 1. Get request for backend data
   const fetchCategory = async () => {
     const response = await fetch(`${API_BASE_URL}${props.location.pathname}${props.location.search}`)
     const normalize = await normalizeResponseErrors(response)
     const rcvdBusinesses = await normalize.json()
-    // let newArr = []
-    console.log(rcvdBusinesses)
-    // rcvdBusinesses.forEach(category => newArr.push(category.charAt(0).toUpperCase() + category.slice(1)))
     setBusinesses(rcvdBusinesses)
-    // How do make it wait for the response to finish and then do the if statement?
   }
   
   useEffect(
@@ -26,6 +21,13 @@ export default function Businesses(props) {
       fetchCategory()
     }, []
   )
+
+  const viewBusiness = (e) => {
+    e.preventDefault()
+    let businessId = e.target.id
+    props.history.push(`/business/${businessId}`)
+  }
+
   // 2. Display Results
   let business;
   let title;
@@ -33,8 +35,13 @@ export default function Businesses(props) {
     title = 'Businesses';
     business = businesses.map((business, index) => {
       return (
-        <div id={business.id} key={index}>
-        
+        <div className='listedBusinesses' key={index}>
+          <h3>{business.name}</h3>
+          <div className='location'>
+            {business.city}
+            {business.state}
+          </div>
+          <input type='button' id={business.id} key={index} value='View Business' onClick={viewBusiness}></input>
         </div>
       )
     })
@@ -43,16 +50,12 @@ export default function Businesses(props) {
     business = 'Submit a business'
   }
 
-  
-  // 3. Handle click on category
-
   return (
     <div className='businesses'>
       <NavBar />
       <Search {...props} />
       <div>
       <h2>{title}</h2>
-      {/* Div will show up even if empty */}
       <div>{business}</div>
       </div>
     </div>
