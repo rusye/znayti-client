480c84407c785b01c90f95746f6ca4c63558a26d
Russ yev <ruslanyev@gmail.com>
2019-03-03 20:50:35 -0800

    added the category component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
 import HomePage from './components/HomePage';
 import Categories from './components/Categories';
+import Category from './components/Category';
 // import Business from './components/Business';
-// import Category from './components/Category';
+
 import * as serviceWorker from './serviceWorker';
 
 function App() {
+  // const [storeUser, setStoreUser]
   return (
     <div>
       <Router>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/business/search' component={Categories} />
-          {/* <Route exact path='/business/:category/search' component={Category} /> */}
+          <Route exact path='/business/:category/search' component={Category} />
           {/* <Route exact path='/business/:id' component={Business} /> */}
         </Switch>
       </Router>
