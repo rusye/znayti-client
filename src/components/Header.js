dcc129f7e148be28bd0644e71fe4ee1877f3b43c
Russ yev <ruslanyev@gmail.com>
2019-03-05 17:47:45 -0800

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+
+export default function Businesses() {
+
+  return (
+    <header className='App-header'>
+      <h1>
+        Znayti - Connecting Russian speakers with Russian speaking businesses 
+      </h1>
+    </header>
+  );
+}
