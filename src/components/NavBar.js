793a513de0a8e4aae30aefb9b5655e204d3aa9d8
Russ yev <ruslanyev@gmail.com>
2019-03-06 17:39:17 -0800

    made an if statement to change around the look of the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,17 @@ import './NavBar.css';
 export default function NavBar(props) {
 
   return (
-    <h2>This Is The Nav Bar <Link to="/">Home</Link></h2>
+    <div>
+      {
+        localStorage.loggedIn ? (<div>
+          <Link to="/">Home</Link>
+          <Link to="/">Dashboard</Link>
+          <Link to="/">LogOut</Link>
+        </div>
+        ) : (
+          <h2>This Is The Nav Bar <Link to="/">Home</Link></h2>
+          )
+      }
+    </div>
   )
 }
