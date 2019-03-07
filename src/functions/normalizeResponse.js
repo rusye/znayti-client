98ae8cda83b8481179e3bc32bbed79b13938b6e1
Russ yev <ruslanyev@gmail.com>
2019-03-07 15:59:37 -0800

    updated res if status is 403

--- src/functions/normalizeResponse.js
+++ src/functions/normalizeResponse.js
@@ -4,15 +4,14 @@
 // containing the status text.  If there is no error then we continue with
 // the promise chain.
 export const normalizeResponseErrors = res => {
+  console.log(res)
   if (!res.ok) {
     if (
         res.headers.has('content-type') &&
         res.headers.get('content-type').startsWith('application/json')
-    ) {
-        // It's a nice JSON error returned by us, so decode it
-        return res.json().then(err => Promise.reject(err));
-    }
-    // It's a less informative error returned by express
+      ) {
+        return res.json().then(err => Promise.reject({code: res.status, err}));
+      }
     return Promise.reject({
         code: res.status,
         message: res.statusText
