5cfcbdb17065a195a1b7e213edd1041dc0ef30b8
Russ yev <ruslanyev@gmail.com>
2019-03-09 00:05:19 -0800

    finaled form and handle submit

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,19 +1,61 @@
 import React, {useState} from 'react';
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from '../functions/normalizeResponse';
 
 export default function AddCategory() {
-  const [categoryName, setCategoryName] = useState('');
+  const [name, setName] = useState('');
+  const [serverMessage, setServerMessage] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault(e);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+
+    return (fetch(`${API_BASE_URL}/categories/`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        name,
+        'user_id': localStorage.userId
+      })
+    }))
+    .then(res => normalizeResponseErrors(res))
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      setServerMessage(null);
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+      let message;
+      if (err.code === 422 || 400) {
+        message = err.message;
+      } else if (err.code === 500) {
+        message = 'Internal server error';
+      } else {
+        message = 'Something went wrong, please try again later';
+      }
+      localStorage.setItem('serverMessage', message)
+      setServerMessage(message)
+    })
+  };
+
   return(
-    <form className='login-form' // onSubmit={handleSubmit} 
-    >
+    <form className='add-category-form' onSubmit={handleSubmit}>
     <label htmlFor='category-name' aria-label='category-name-input'>Category Name</label>
       <input
-        value={categoryName}
-        onChange={e => setCategoryName(e.target.value)}
-        placeholder='enter category name'
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder='Category Name'
         type='text'
-        name='categoryName'
-        pattern='[A-Za-z0-9_]{1,15}'
-        title='Please enter a desired category name'
+        name='category'
+        pattern='^(\b[A-Z]\w*\s*)+.{2,}$'
+        title='Please enter category name that is 3+ characters, first letters are capitalized'
         id='category-name'
         required
         aria-labelledby='category-name'
@@ -22,6 +64,7 @@ export default function AddCategory() {
       <button type='submit' className='category-submit'>
         Submit
       </button>
+      {serverMessage}
     </form>
   )
 }
