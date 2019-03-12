a7abff1904e16d8011c75443f09a41b629dad70f
Russ yev <ruslanyev@gmail.com>
2019-03-12 01:53:03 -0700

    updated context and commented it out, creates infinit loop in useEffect

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { API_BASE_URL } from '../config';
 import { normalizeResponseErrors } from '../functions/normalizeResponse';
+// import CategoryContext from "./Context";
 
-export default function AddCategory() {
+export default function AddCategory(props) {
   const [name, setName] = useState('');
   const [serverMessage, setServerMessage] = useState(null);
 
@@ -10,8 +11,11 @@ export default function AddCategory() {
     setName('')
   }
 
+  // const allCategories = useContext(CategoryContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(allCategories)
 
     const headers = {
       'Content-Type': 'application/json',
@@ -32,9 +36,12 @@ export default function AddCategory() {
     })
     .then(res => {
       setServerMessage(null);
+      // allCategories.push(res)
+      props.updateCategories(res)
       reset()
       setServerMessage(`${res.name} succesfully added`)
       setInterval(() => { setServerMessage(null) }, 4000)
+      // console.log(allCategories)
     })
     .catch(err => {
       console.log(err)
