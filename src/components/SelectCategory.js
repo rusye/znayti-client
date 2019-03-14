dd195235c97abebc18e5664650dfa5a04de30524
Russ yev <ruslanyev@gmail.com>
2019-03-13 22:59:55 -0700

    initial commit

---
+++ src/components/SelectCategory.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+
+export default function SelectCategory(props) {
+  const [categories, setCategories] = useState('')
+  const [select, setSelect] = useState('')
+
+  const populateCategories = () => {
+    if (props.categories.length > 0) {
+      setSelect('-- select a category --')
+      setCategories(props.categories.map((categoryDetails, index) => {
+        return (
+          <option key={index} value={categoryDetails.id}>{categoryDetails.name}</option>
+        )
+      }))
+    } else {
+      setSelect('Create a category')
+    }
+  }
+
+  useEffect(
+    () => {
+      populateCategories()
+    }, [props.categories]
+  )
+
+  return (
+    <select value={props.category} onChange={e => props.setCategory(e.target.value)} required>
+      <option disabled={true} value=''>{select}</option>
+      {categories}
+    </select>
+  );
+}
