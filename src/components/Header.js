6bc363fff0baa732cf3bad063cff22c68b2e223d
Russ yev <ruslanyev@gmail.com>
2019-03-20 15:15:52 -0700

    seperated the name from business statment

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ export default function Header() {
 
   return (
     <header className='App-header'>
-      <h1>
-        Znayti - Connecting Russian speakers with Russian speaking businesses 
-      </h1>
+      <h1>Znayti</h1>
+      <h2>Connecting Russian speakers with Russian speaking businesses</h2>
     </header>
   );
 }
