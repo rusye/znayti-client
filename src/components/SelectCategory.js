68d101faf22cc33f26195dc63c75a2de3a337694
Russ yev <ruslanyev@gmail.com>
2019-03-28 18:39:37 -0700

    added padding around labels

--- src/components/SelectCategory.js
+++ src/components/SelectCategory.js
@@ -1,32 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function SelectCategory(props) {
-  const [categories, setCategories] = useState('')
-  const [select, setSelect] = useState('')
+  const [categories, setCategories] = useState("");
+  const [select, setSelect] = useState("");
 
   const populateCategories = () => {
     if (props.categories.length > 0) {
-      setSelect('-- select a category --')
-      setCategories(props.categories.map((categoryDetails, index) => {
-        return (
-          <option key={index} value={categoryDetails.id}>{categoryDetails.name}</option>
-        )
-      }))
+      setSelect("-- select a category --");
+      setCategories(
+        props.categories.map((categoryDetails, index) => {
+          return (
+            <option key={index} value={categoryDetails.id}>
+              {categoryDetails.name}
+            </option>
+          );
+        })
+      );
     } else {
-      setSelect('Create a category')
+      setSelect("Create a category");
     }
-  }
+  };
 
-  useEffect(
-    () => {
-      populateCategories()
-    }, [props.categories]
-  )
+  useEffect(() => {
+    populateCategories();
+  }, [props.categories]);
 
   return (
-    <label aria-label='select-category'>Select Category
-      <select value={props.category} onChange={e => props.setCategory(e.target.value)} required>
-        <option disabled={true} value=''>{select}</option>
+    <label aria-label="select-category">
+      Select Category&nbsp;
+      <select
+        value={props.category}
+        onChange={e => props.setCategory(e.target.value)}
+        required
+      >
+        <option disabled={true} value="">
+          {select}
+        </option>
         {categories}
       </select>
     </label>
