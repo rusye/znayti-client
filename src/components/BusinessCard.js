35276f5c9505facd11f8c5fbb2226f37d195e552
Russ yev <ruslanyev@gmail.com>
2019-03-29 14:10:13 -0700

    added contactName

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -11,6 +11,7 @@ export default function BusinessCard(props) {
       onClick={props.viewBusiness}
     >
       <h3 id={props.business.id}>{props.business.name}</h3>
+      {props.business.contactName ? <span id={props.business.id} className='contactName' >{props.business.contactName}</span> : null }
       <span id={props.business.id} className="location">
         {props.business.city}, &nbsp;{props.business.state}
       </span>
