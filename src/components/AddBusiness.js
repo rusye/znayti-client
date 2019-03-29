ce4e898dae4109935cdd6060b50aa16d2fb152cd
Russ yev <ruslanyev@gmail.com>
2019-03-29 13:57:47 -0700

    added contactName and googlePlace

--- src/components/AddBusiness.js
+++ src/components/AddBusiness.js
@@ -1,130 +1,131 @@
-import React, {useState} from 'react';
-import { API_BASE_URL } from '../config';
-import BusinessForm from './BusinessForm'
-import { normalizeResponseErrors } from '../functions/normalizeResponse';
+import React, { useState } from "react";
+import { API_BASE_URL } from "../config";
+import BusinessForm from "./BusinessForm";
+import { normalizeResponseErrors } from "../functions/normalizeResponse";
 
 export default function AddBusiness(props) {
-  const [businessName, setBusinessName] = useState('');
-  const [category, setCategory] = useState('');
-  const [telephone, setTelephone] = useState('')
-  const [street, setStreet] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [zip, setZip] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [hours, setHours] = useState({})
-  const [resetHours, setResetHours] = useState(false)
+  const [businessName, setBusinessName] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [category, setCategory] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [googlePlace, setGooglePlace] = useState("");
+  const [hours, setHours] = useState({});
+  const [resetHours, setResetHours] = useState(false);
   const [serverMessage, setServerMessage] = useState(null);
 
   const reset = () => {
-    setBusinessName('')
-    setCategory('')
-    setTelephone('')
-    setStreet('')
-    setCity('')
-    setState('')
-    setZip('')
-    setLatitude('')
-    setLongitude('')
-    setHours({})
-    setResetHours(true)
-  }
+    setBusinessName("");
+    setCategory("");
+    setTelephone("");
+    setStreet("");
+    setCity("");
+    setState("");
+    setZip("");
+    setLatitude("");
+    setLongitude("");
+    setGooglePlace("");
+    setHours({});
+    setResetHours(true);
+  };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
     const headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      "Content-Type": "application/json",
+      Accept: "application/json"
     };
 
-    return (fetch(`${API_BASE_URL}/business/`, {
-      method: 'POST',
+    return fetch(`${API_BASE_URL}/business/`, {
+      method: "POST",
       headers,
       body: JSON.stringify({
-        'user_id': localStorage.userId,
-        'name': businessName,
+        user_id: localStorage.userId,
+        name: businessName,
+        contactName,
         category,
-        'address': {
+        address: {
           street,
           city,
           state,
           zip,
-          'coordinates': [longitude, latitude]
+          coordinates: [longitude, latitude]
         },
-        "hours": hours,
+        googlePlace,
+        hours: hours,
         telephone
       })
-    }))
-    .then(res => normalizeResponseErrors(res))
-    .then(res => {
-      return res.json();
     })
-    .then(res => {
-      setServerMessage(null);
-      reset()
-      setServerMessage(`${res.name} succesfully added`)
-      setTimeout(() => { setServerMessage(null) }, 4000)
-    })
-    .catch(err => {
-      let message;
-      if (err.code === 422 || 400) {
-        message = err.message;
+      .then(res => normalizeResponseErrors(res))
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setServerMessage(null);
+        reset();
+        setServerMessage(`${res.name} succesfully added`);
+        setTimeout(() => {
+          setServerMessage(null);
+        }, 4000);
+      })
+      .catch(err => {
+        let message;
+        if (err.code === 422 || 400) {
+          message = err.message;
         } else if (err.code === 500) {
-          message = 'Internal server error';
+          message = "Internal server error";
         } else {
-          message = 'Something went wrong, please try again later';
+          message = "Something went wrong, please try again later";
         }
-      setServerMessage(message)
-    })
+        setServerMessage(message);
+      });
   };
 
   const handleHoursChange = (day, value) => {
-    hours[day] = value
-  }
-  
-  return(
-    <form className='add-business-form' onSubmit={handleSubmit}>
+    hours[day] = value;
+  };
+
+  return (
+    <form className="padding " onSubmit={handleSubmit}>
       <fieldset>
         <legend>Add New Business</legend>
         <BusinessForm
           {...props}
-
           handleHoursChange={handleHoursChange}
-
           businessName={businessName}
           setBusinessName={setBusinessName}
-
+          contactName={contactName}
+          setContactName={setContactName}
           category={category}
           setCategory={setCategory}
-
           telephone={telephone}
           setTelephone={setTelephone}
-
           street={street}
           setStreet={setStreet}
-
           city={city}
           setCity={setCity}
-
           state={state}
           setState={setState}
-
           zip={zip}
           setZip={setZip}
-
           latitude={latitude}
           setLatitude={setLatitude}
-
           longitude={longitude}
           setLongitude={setLongitude}
-
+          googlePlace={googlePlace}
+          setGooglePlace={setGooglePlace}
           resetHours={resetHours}
+          reset={reset}
           setResetHours={setResetHours}
         />
-        {serverMessage}
+        {serverMessage ? <div>{serverMessage}</div> : null}
       </fieldset>
     </form>
-  )
+  );
 }
