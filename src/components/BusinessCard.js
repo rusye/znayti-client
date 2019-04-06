e49e33afa89a9dff5da50f2eb481f3f7a9cc9f43
Russ yev <ruslanyev@yahoo.com>
2019-04-05 18:43:20 -0700

    added styling to business and category buttons

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -4,14 +4,18 @@ import "./BusinessCard.css";
 export default function BusinessCard(props) {
   return (
     <button
-      className="businessCard"
+      className="btn btn--border btn--primary btn--animated businessCard"
       type="button"
       id={props.business.id}
       key={props.business.id}
       onClick={props.viewBusiness}
     >
       <h3 id={props.business.id}>{props.business.name}</h3>
-      {props.business.contactName ? <span id={props.business.id} className='contactName' >{props.business.contactName}</span> : null }
+      {props.business.contactName ? (
+        <span id={props.business.id} className="contactName">
+          {props.business.contactName}
+        </span>
+      ) : null}
       <span id={props.business.id} className="location">
         {props.business.city}, &nbsp;{props.business.state}
       </span>
