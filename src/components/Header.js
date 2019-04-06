0cb86f8a33fa06a0aabcdb9a3542bc1465b662ef
Russ yev <ruslanyev@yahoo.com>
2019-04-05 18:50:52 -0700

    updated business statement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './Header.css';
-
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
-
   return (
-    <header className='App-header'>
+    <header className="App-header">
       <h1>Znayti</h1>
-      <h2>Connecting Russian speakers with Russian speaking businesses</h2>
+      <h2>
+        Connecting you with businesses that have a Russian speaker on staff
+      </h2>
     </header>
   );
 }
