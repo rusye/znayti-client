87920d077771c71fb3c9337ffeb85fcf5d981bc1
Russ yev <ruslanyev@yahoo.com>
2019-04-05 18:38:30 -0700

    changed form display to state, added styling to form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,8 @@ import { Redirect } from "react-router-dom";
 import { API_BASE_URL } from "../config";
 import { normalizeResponseErrors } from "../functions/normalizeResponse";
 import "./Dashboard.css";
+import plusIcon from "../images/plus.svg";
+import minusIcon from "../images/minus.svg";
 import NavBar from "./NavBar";
 import AddCategory from "./AddCategory";
 import AddAdminUser from "./AddAdminUser";
@@ -12,6 +14,11 @@ import DeleteCategory from "./DeleteCategory";
 
 export default function Dashboard() {
   const [categories, setCategories] = useState("");
+  const [displayAddCategory, setDisplayAddCategory] = useState(false);
+  const [displayAddBusiness, setDisplayAddBusiness] = useState(false);
+  const [displayAddAdminUser, setDisplayAddAdminUser] = useState(false);
+  const [displayEditCategory, setDisplayEditCategory] = useState(false);
+  const [displayDeleteCategory, setDisplayDeleteCategory] = useState(false);
   const [serverMessage, setServerMessage] = useState(null);
   const [triggerFetch, setTriggerFetch] = useState(1);
 
@@ -44,56 +51,119 @@ export default function Dashboard() {
     }
   }, [triggerFetch]);
 
-  const collapsible = () => {
-    const coll = document.getElementsByClassName("collapsible");
-
-    for (let i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.height) {
-          content.style.height = null;
-        } else {
-          content.style.height = "100%"
-        }
-      });
-    }
-  };
-
-  useEffect(() => {
-    collapsible();
-  }, []);
-
   return !localStorage.loggedIn ? (
     <Redirect to="/bigboss/login" />
   ) : (
     <>
       <NavBar />
       <section className="fullCentered componentLayout">
-        <button className="collapsible">Add Category</button>
-        <div className="content white">
-          <AddCategory updateCategories={updateCategories} />
-        </div>
+        <button
+          className="collapsible"
+          onClick={() =>
+            displayAddCategory
+              ? setDisplayAddCategory(false)
+              : setDisplayAddCategory(true)
+          }
+        >
+          Add Category
+          <img
+            className="plusMinusIcon"
+            src={displayAddCategory ? minusIcon : plusIcon}
+            alt="toggle sign"
+          />
+        </button>
+        {displayAddCategory ? (
+          <div className="content">
+            <AddCategory updateCategories={updateCategories} />
+          </div>
+        ) : null}
 
-        <button className="collapsible">Add Business</button>
-        <div className="content white">
-          <AddBusiness categories={categories} />
-        </div>
+        <button
+          className="collapsible"
+          onClick={() =>
+            displayAddBusiness
+              ? setDisplayAddBusiness(false)
+              : setDisplayAddBusiness(true)
+          }
+        >
+          Add Business
+          <img
+            className="plusMinusIcon"
+            src={displayAddBusiness ? minusIcon : plusIcon}
+            alt="toggle sign"
+          />
+        </button>
+        {displayAddBusiness ? (
+          <div className="content">
+            <AddBusiness categories={categories} />
+          </div>
+        ) : null}
 
-        <button className="collapsible">Add Admin User</button>
-        <div className="content white">
-          <AddAdminUser />
-        </div>
+        <button
+          className="collapsible"
+          onClick={() =>
+            displayAddAdminUser
+              ? setDisplayAddAdminUser(false)
+              : setDisplayAddAdminUser(true)
+          }
+        >
+          Add Admin User
+          <img
+            className="plusMinusIcon"
+            src={displayAddAdminUser ? minusIcon : plusIcon}
+            alt="toggle sign"
+          />
+        </button>
+        {displayAddAdminUser ? (
+          <div className="content">
+            <AddAdminUser />
+          </div>
+        ) : null}
 
-        <button className="collapsible">Edit Category</button>
-        <div className="content white">
-          <EditCategory categories={categories} onChange={setTriggerFetch} />
-        </div>
+        <button
+          className="collapsible"
+          onClick={() =>
+            displayEditCategory
+              ? setDisplayEditCategory(false)
+              : setDisplayEditCategory(true)
+          }
+        >
+          Edit Category
+          <img
+            className="plusMinusIcon"
+            src={displayEditCategory ? minusIcon : plusIcon}
+            alt="toggle sign"
+          />
+        </button>
+        {displayEditCategory ? (
+          <div className="content">
+            <EditCategory categories={categories} onChange={setTriggerFetch} />
+          </div>
+        ) : null}
 
-        <button className="collapsible">Delete Category</button>
-        <div className="content white">
-          <DeleteCategory categories={categories} onChange={setTriggerFetch} />
-        </div>
+        <button
+          className="collapsible"
+          onClick={() =>
+            displayDeleteCategory
+              ? setDisplayDeleteCategory(false)
+              : setDisplayDeleteCategory(true)
+          }
+        >
+          Delete Category
+          <img
+            className="plusMinusIcon"
+            src={displayDeleteCategory ? minusIcon : plusIcon}
+            alt="toggle sign"
+          />
+        </button>
+        {displayDeleteCategory ? (
+          <div className="content">
+            <DeleteCategory
+              categories={categories}
+              onChange={setTriggerFetch}
+            />
+          </div>
+        ) : null}
 
         {serverMessage}
       </section>
