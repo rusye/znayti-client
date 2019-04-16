eeb956b8080f00069b1f39a9bf40d2d9fb097aa4
Russ yev <ruslanyev@yahoo.com>
2019-04-16 00:25:03 -0700

    cleaned up code, updated response upon successful submission

--- src/components/SubmitABusinessForm.js
+++ src/components/SubmitABusinessForm.js
@@ -11,6 +11,8 @@ export default function SubmitABusinessForm(props) {
   const [newBusinessHours, setNewBusinessHours] = useState("");
   const [newBusinessTelephone, setNewBusinessTelephone] = useState("");
   const [comment, setComment] = useState("");
+  const [submitDisable, setSubmitDisable] = useState(false);
+  const [success, setSuccess] = useState(false);
   const [serverMessage, setServerMessage] = useState(null);
 
   const reset = () => {
@@ -25,7 +27,7 @@ export default function SubmitABusinessForm(props) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    // disable submit button on click
+    setSubmitDisable(true);
 
     const headers = {
       "Content-Type": "application/json",
@@ -36,7 +38,6 @@ export default function SubmitABusinessForm(props) {
       method: "POST",
       headers,
       body: JSON.stringify({
-        //all the fields
         formID: "add",
         replyTo,
         submitterName,
@@ -54,17 +55,17 @@ export default function SubmitABusinessForm(props) {
       .then(res => {
         setServerMessage(null);
         reset();
-        setServerMessage("Business succesfully submitted");
+        setSubmitDisable(false);
+        setSuccess(true);
         setTimeout(() => {
-          setServerMessage(null);
+          setSuccess(false);
           props.updateModal();
-          // enable the submit button
-        }, 4000);
+        }, 3000);
       })
       .catch(err => {
         console.log(err);
-        //enable submit button if something is wrong
         let message = "Something went wrong, please try again later";
+        setSubmitDisable(false);
         setServerMessage(message);
         setTimeout(() => {
           setServerMessage(null);
@@ -74,131 +75,141 @@ export default function SubmitABusinessForm(props) {
 
   return (
     <section className="modal forms" aria-live="assertive">
-      <form className="modal-content animate padding" onSubmit={handleSubmit}>
-        <div className="imgcontainer">
-          <span
-            className="close"
-            onClick={props.updateModal}
-            title="Close Form"
-          >
-            &times;
-          </span>
-        </div>
+      <div className="modal-content animate padding">
+        {!success ? (
+          <form onSubmit={handleSubmit}>
+            <div className="imgcontainer">
+              <span
+                className="close"
+                onClick={props.updateModal}
+                title="Close Form"
+              >
+                &times;
+              </span>
+            </div>
 
-        <fieldset>
-          <legend className="formTitle">Submit A Business</legend>
-          <label aria-label="your name">
-            Your Name&nbsp;
-            <input
-              value={submitterName}
-              onChange={e => setSubmitterName(e.target.value)}
-              placeholder="enter your name"
-              type="text"
-              name="your-name"
-              title="Enter your name"
-              aria-labelledby="your-name"
-              required
-            />
-          </label>
+            <fieldset>
+              <legend className="formTitle">Submit A Business</legend>
+              <label aria-label="your name">
+                Your Name&nbsp;
+                <input
+                  value={submitterName}
+                  onChange={e => setSubmitterName(e.target.value)}
+                  placeholder="enter your name"
+                  type="text"
+                  name="your-name"
+                  title="Enter your name"
+                  aria-labelledby="your-name"
+                  required
+                />
+              </label>
 
-          <label aria-label="your email">
-            Your Email&nbsp;
-            <input
-              value={replyTo}
-              onChange={e => setReplyTo(e.target.value)}
-              placeholder="enter your email"
-              type="email"
-              name="your-email"
-              title="Enter your email so we can contact you example@email.com"
-              aria-labelledby="your-name"
-              required
-            />
-          </label>
+              <label aria-label="your email">
+                Your Email&nbsp;
+                <input
+                  value={replyTo}
+                  onChange={e => setReplyTo(e.target.value)}
+                  placeholder="enter your email"
+                  type="email"
+                  name="your-email"
+                  title="Enter your email so we can contact you example@email.com"
+                  aria-labelledby="your-name"
+                  required
+                />
+              </label>
 
-          <label aria-label="business name">
-            Business Name&nbsp;
-            <input
-              value={businessToAdd}
-              onChange={e => setBusinessToAdd(e.target.value)}
-              placeholder="enter business name"
-              type="text"
-              name="business-name"
-              title="Please enter the business name"
-              aria-labelledby="business-name"
-              required
-            />
-          </label>
+              <label aria-label="business name">
+                Business Name&nbsp;
+                <input
+                  value={businessToAdd}
+                  onChange={e => setBusinessToAdd(e.target.value)}
+                  placeholder="enter business name"
+                  type="text"
+                  name="business-name"
+                  title="Please enter the business name"
+                  aria-labelledby="business-name"
+                  required
+                />
+              </label>
 
-          <label aria-label="telephone-number">
-            Business Telephone&nbsp;
-            <input
-              type="tel"
-              value={newBusinessTelephone}
-              onChange={e => setNewBusinessTelephone(e.target.value)}
-              placeholder="5031239876"
-              title="Please enter the business telephone number"
-              name="telephone"
-              aria-labelledby="telephone"
-              required
-            />
-          </label>
+              <label aria-label="telephone-number">
+                Business Telephone&nbsp;
+                <input
+                  type="tel"
+                  value={newBusinessTelephone}
+                  onChange={e => setNewBusinessTelephone(e.target.value)}
+                  placeholder="5031239876"
+                  title="Please enter the business telephone number"
+                  name="telephone"
+                  aria-labelledby="telephone"
+                  required
+                />
+              </label>
 
-          <label className="userTextare" aria-label="business address">
-            Business Address&nbsp;
-            <textarea
-              rows={2}
-              value={newBusinessAddress}
-              onChange={e => setNewBusinessAddress(e.target.value)}
-              placeholder="enter business address"
-              type="text"
-              name="business-address"
-              title="Please enter the business address"
-              aria-labelledby="business-address"
-              required
-            />
-          </label>
+              <label className="userTextare" aria-label="business address">
+                Business Address&nbsp;
+                <textarea
+                  rows={2}
+                  value={newBusinessAddress}
+                  onChange={e => setNewBusinessAddress(e.target.value)}
+                  placeholder="enter business address"
+                  type="text"
+                  name="business-address"
+                  title="Please enter the business address"
+                  aria-labelledby="business-address"
+                  required
+                />
+              </label>
 
-          <label className="userTextare" aria-label="business hours">
-            Business Hours&nbsp;
-            <textarea
-              rows={2}
-              value={newBusinessHours}
-              onChange={e => setNewBusinessHours(e.target.value)}
-              placeholder="enter business operating hours"
-              type="text"
-              name="business-hours"
-              title="Please enter the business operating hours"
-              aria-labelledby="business-address"
-              required
-            />
-          </label>
+              <label className="userTextare" aria-label="business hours">
+                Business Hours&nbsp;
+                <textarea
+                  rows={2}
+                  value={newBusinessHours}
+                  onChange={e => setNewBusinessHours(e.target.value)}
+                  placeholder="enter business operating hours"
+                  type="text"
+                  name="business-hours"
+                  title="Please enter the business operating hours"
+                  aria-labelledby="business-address"
+                  required
+                />
+              </label>
 
-          <label className="userTextare" aria-label="comment">
-            Comment&nbsp;
-            <textarea
-              rows={3}
-              value={comment}
-              onChange={e => setComment(e.target.value)}
-              placeholder="Is there a specific person you want users to contact?  Any additional info you would like to add?"
-              type="text"
-              name="comment"
-              title="Please enter any additional comments you might have"
-              aria-labelledby="comment"
-            />
-          </label>
-        </fieldset>
+              <label className="userTextare" aria-label="comment">
+                Comment&nbsp;
+                <textarea
+                  rows={3}
+                  value={comment}
+                  onChange={e => setComment(e.target.value)}
+                  placeholder="Is there a specific person you want users to contact?  Any additional info you would like to add?"
+                  type="text"
+                  name="comment"
+                  title="Please enter any additional comments you might have"
+                  aria-labelledby="comment"
+                />
+              </label>
+            </fieldset>
 
-        {/* Make the spinning thing show up when they hit submit */}
-        <button type="submit" className="add-business-submit">
-          Submit
-        </button>
+            {/* Make the spinning thing show up when they hit submit */}
+            <button
+              type="submit"
+              disabled={submitDisable}
+              className="add-business-submit"
+            >
+              Submit
+            </button>
 
-        <button type="reset" onClick={reset}>
-          Reset
-        </button>
+            <button type="reset" onClick={reset}>
+              Reset
+            </button>
 
-        {serverMessage ? <div>{serverMessage}</div> : null}
-      </form>
+            {serverMessage ? <div>{serverMessage}</div> : null}
+          </form>
+        ) : (
+          <p>Business succesfully submitted</p>
+        )}
+      </div>
     </section>
   );
 }
