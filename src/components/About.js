7aca2100cdf64e6a65a0dc095da04a623698e258
Russ yev <ruslanyev@yahoo.com>
2019-04-23 15:29:12 -0700

    updated layout of about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,32 +4,34 @@ import NavBar from "./NavBar";
 
 export default function About() {
   return (
-    <div className="fullCentered about">
+    <div className="fullCentered navPadding">
       <NavBar />
-      <span className="missionTitle">Our Mission:</span>
-      <span className="statment">
-        Helping you connect with a business that can communicate with you in
-        Russian.
-      </span>
+      <div className="about">
+        <h1 className="missionTitle">Our Mission:</h1>
+        <p className="statment">
+          Helping you connect with a business that can communicate with you in
+          Russian.
+        </p>
 
-      <h1>About Znayti</h1>
-      <p>
-        Znayti was founded because of the frustration that ensued after trying
-        to locate a body shop but all we had to go off was word of mouth and no
-        one had the phone number of the business on hand without having to call
-        around. Currently we are focused on serving Portland, OR.
-      </p>
+        <h2>About Znayti</h2>
+        <p>
+          Znayti was founded because of the frustration that ensued after trying
+          to locate a body shop but all we had to go off was word of mouth and
+          no one had the phone number of the business on hand without having to
+          call around. Currently we are focused on serving Portland, OR.
+        </p>
 
-      <h2>Current Goal</h2>
-      <p>
-        We are currently creating a directory of businesses in the Portland, OR
-        area that have a Russian speaker on staff.
-      </p>
+        <h2>Current Goal</h2>
+        <p>
+          We are currently creating a directory of businesses in the Portland,
+          OR area that have a Russian speaker on staff.
+        </p>
 
-      <h2>Next Goal</h2>
-      <p>
-        Add a map that that will show you all the listings within a category.
-      </p>
+        <h2>Next Goal</h2>
+        <p>
+          Add a map that that will show you all the listings within a category.
+        </p>
+      </div>
     </div>
   );
 }
